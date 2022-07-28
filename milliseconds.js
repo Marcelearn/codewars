4d5179d18f6393362b5bd71d1f956defce29c553
Marcelearn <jaworskimarcel@gmail.com>
2022-07-28 15:26:25 +0200

    bonusTime

--- milliseconds.js
+++ milliseconds.js
@@ -1,8 +1,8 @@
 
 function milliseconds(stunde, minute, secunde){
-    const s = 1000
-    const m = s * 60;
     const h = m *60;
+    const s = 1000;
+    const m = s * 60;
     return (h*stunde) + (m * minute) + (secunde * s);
 }
-console.log(milliseconds(01, 02, 1))
+console.log(milliseconds(01, 02, 1)) 
