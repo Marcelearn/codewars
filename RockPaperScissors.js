e04993ecdd94a6e2d8b95d0d57955751989796d5
Marcelearn <jaworskimarcel@gmail.com>
2022-07-29 12:25:54 +0200

    refactor

--- RockPaperScissors.js
+++ RockPaperScissors.js
@@ -8,17 +8,20 @@ const rps = (p1, p2) => {
     if(p1 == "rock" && p2 == "paper"){
       return "Player 2 won!"
     }
-      if(p1 == "scissors" && p2 == "paper"){
-      return "Player 1 won!"
-    }
-    if(p1 == "rock" && p2 == "scissors"){
-      return "Player 1 won!"
-    }
-    if(p1 == "paper" && p2 == "rock"){
-      return "Player 1 won!"
-    }
+    //   if(p1 == "scissors" && p2 == "paper"){
+    //   return "Player 1 won!"
+    // }
+    // if(p1 == "rock" && p2 == "scissors"){
+    //   return "Player 1 won!"
+    // }
+    // if(p1 == "paper" && p2 == "rock"){
+    //   return "Player 1 won!"
+    // }
     if(p1 === p2){
-      return "Draw!"
+        return "Draw!"
+    }
+    else{
+        return "Player 1 won!"
     }
     
   };
